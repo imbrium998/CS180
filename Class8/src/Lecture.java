8cc5dc0c742e6fdbf953309c47ac3d01770a4bc5
imbrium998 <davidklink@Vermillion.local>
2013-10-30 19:27:47 -0400

    midclass

---
+++ Class8/src/Lecture.java
@@ -0,0 +1,8 @@
+
+public class Lecture {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- Class8/src/MidtermPost.java
+++ Class8/src/MidtermPost.java
@@ -1,9 +1,39 @@
+import java.util.Scanner;
+
 
 public class MidtermPost {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner kb = new Scanner(System.in);
+		boolean done = false;
+		
+		while (!done)
+		{
+			String invalue = kb.next();
+			
+			if(invalue.toUpperCase().equals("exit"))
+			{
+				done = true; 
+			}
+		else
+		{
+			int first = Integer.parseInt(invalue);
+			int second = kb.nextInt();
+			int third = kb.nextInt();
+			
+			if ((first > second) && (second > third) ||
+					(first < second) && (second < third))
+			{
+				System.out.println("Numbers are in order");
+				done = true;
+			}
+			
+			
+		}
+				
+		}
+		
 	}
 
 }
