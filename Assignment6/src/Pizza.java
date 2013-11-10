5d4b48b60b41aab2ae4dae89515d08a1f12c7241
imbrium998 <davidklink@hotmail.com>
2013-11-10 14:04:18 -0500

    Midday sunday afternoon.
    Still need to work out getting the data into the array

--- Assignment6/src/Pizza.java
+++ Assignment6/src/Pizza.java
@@ -2,24 +2,48 @@
 public class Pizza 
 {
 	private char size;
+	private int status;
+	private String[] toppings;
 		
+	// Create get methods for class private variables (Accessor methods)
+		public char getSize() {
+			return this.size;
+		}
 	
-	public char getSize() {
-		return this.size;
+		public int getStatus() {
+			return this.status;
+		}
 		
-	}
-	
-	public boolean setSize(char size)
-	{
-		boolean isValid = false;
-		if ((size == 'S') || (size == 'M') || (size == 'L'))  
-			{ isValid = true;} 
-		else {isValid = false;}
-		return isValid;
-	}
-		
-		
+		public String[] getToppings() {
+			return this.toppings;
+		}
 	
 		
-	
+		
+	// Create methods used 
+		public boolean setSize(char size)
+		{
+			boolean isValid = false;
+			if ((size == 'S') || (size == 'M') || (size == 'L')) 	{ isValid = true; } 
+			else {isValid = false;}
+		
+			return isValid;
+		}
+		
+		
+		public boolean setStatus(char status)
+		{
+			boolean isValid = false;
+			if ((status == 0) || (status == 1) || (status == 2))	{ isValid = true; }
+			else {isValid = false;}
+			return isValid;
+		}
+		
+		
+		public void setToppings(String [] t)
+		{
+			toppings = t;
+		}
+		
+		
 }
--- Assignment6/src/TestPizza2.java
+++ Assignment6/src/TestPizza2.java
@@ -8,6 +8,7 @@ public class TestPizza2 {
 	 */
 	public static void main(String[] args) {
 		
+		int numToppings = 0;
 		Pizza pizza1 = new Pizza();
 		
 		Scanner kb = new Scanner(System.in);
@@ -16,11 +17,26 @@ public class TestPizza2 {
 		String s = kb.nextLine();
 		char cs = s.charAt(0);
 		
+		System.out.print("How many toppings would you like to add to the pizza? ");
+		numToppings = kb.nextInt();
 		
-	
+		
+		for (int i = 0; i <= numToppings; i++)
+		{
+						System.out.println("Please enter topping number " + (i + 1));
+						String tops = kb.nextLine();
+					
+						pizza1.setToppings(add(tops));
+						
+						
+		}
+		
+		
 		System.out.println(pizza1.setSize(cs));
 		
-	
+		
+		
+		
 		
 		kb.close();
 
